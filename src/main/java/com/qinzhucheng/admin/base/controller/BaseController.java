c3912bc28786254ebf4048f435738b20b43c809c
秦祝成 <qinzhucheng@inspur.com>
2019-12-10 17:10:46 +0800

    测试swagger2通过

--- src/main/java/com/qinzhucheng/admin/base/controller/BaseController.java
+++ src/main/java/com/qinzhucheng/admin/base/controller/BaseController.java
@@ -1,16 +1,20 @@
 package com.qinzhucheng.admin.base.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class BaseController {
 
     private final static Logger logger = LoggerFactory.getLogger(BaseController.class);
 
+    @ApiOperation("hello")
     @RequestMapping("/")
     public String hello() {
         logger.info("测试日志");
--- src/main/java/com/qinzhucheng/admin/configuration/SwaggerConfiguration.java
+++ src/main/java/com/qinzhucheng/admin/configuration/SwaggerConfiguration.java
@@ -1,7 +1,10 @@
 package com.qinzhucheng.admin.configuration;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableSwaggerBootstrapUi;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
