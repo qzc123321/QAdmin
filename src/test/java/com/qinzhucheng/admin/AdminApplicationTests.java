90e817ee293e63a731095b3f045d41ff73049d4e
qinzhucheng <qinzhucheng@inspur.com>
2020-05-30 15:54:10 +0800

    修复测试不通过问题

--- src/test/java/com/qinzhucheng/admin/AdminApplicationTests.java
+++ src/test/java/com/qinzhucheng/admin/AdminApplicationTests.java
@@ -3,7 +3,7 @@ package com.qinzhucheng.admin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AdminApplicationTests {
 
     @Test
